ac013eba89dc5f10eb01da70ee4dea4aac2dd49a
Ori Tabac (Mend.io) <84838519+tabacmend@users.noreply.github.com>
2024-08-22 15:06:17 +0300

    Update MendSASTScan.groovy

--- vars/MendSASTScan.groovy
+++ vars/MendSASTScan.groovy
@@ -5,13 +5,13 @@ def call() {
       export repo=$(basename -s .git $(git config --get remote.origin.url))
       export branch=$(git rev-parse --abbrev-ref HEAD)
       export MEND_SAST_REPORT_TYPE="OWASP2021"
-      ./mend code --non-interactive -s "*//${JOB_NAME}//${repo}_${branch}" -r --formats html --filename code-results
+      ./mend code --non-interactive -s "*//${JOB_NAME}//${repo}_${branch}" -r --formats pdf --filename code-results
       if [[ "$code_exit" == "9" ]]; then
         echo "[warning] Code scan threshold violation"
       else
         echo "No policy violations found in code scan"
       fi
       '''
-   archiveArtifacts artifacts: "code-results.html", fingerprint: true
+   archiveArtifacts artifacts: "code-results.pdf", fingerprint: true
  }
 }
