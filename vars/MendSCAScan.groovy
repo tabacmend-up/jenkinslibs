e11e932a32ae05f05acbabe19f3ca83787827881
Ori Tabac (Mend.io) <84838519+tabacmend@users.noreply.github.com>
2024-09-03 20:17:12 +0300

    Update MendSCAScan.groovy

--- vars/MendSCAScan.groovy
+++ vars/MendSCAScan.groovy
@@ -14,8 +14,6 @@ def call(boolean Reachability) {
             ./mend dep -u -s "*//${JOB_NAME}//${repo}_${branch}" --fail-policy --non-interactive --export-results dep-results.txt'
             '''
         }
-    }
-}
 
             // export repo=$(basename -s .git $(git config --get remote.origin.url))
             // export branch=$(git rev-parse --abbrev-ref HEAD)
