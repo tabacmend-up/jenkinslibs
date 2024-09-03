705ef34024017272588059c4f73c7dfe5cfd77a7
Ori Tabac (Mend.io) <84838519+tabacmend@users.noreply.github.com>
2024-09-03 20:22:10 +0300

    Update MendSCAScan.groovy

--- vars/MendSCAScan.groovy
+++ vars/MendSCAScan.groovy
@@ -3,33 +3,24 @@ def call(boolean Reachability) {
     catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
         if (Reachability) {
             sh """
-            export repo=$(basename -s .git $(git config --get remote.origin.url))
-            export branch=$(git rev-parse --abbrev-ref HEAD)
-            ./mend dep -u -r -s "*//${JOB_NAME}//${repo}_${branch}" --fail-policy --non-interactive --export-results dep-results.txt'
+            export repo=\$(basename -s .git \$(git config --get remote.origin.url))
+            export branch=\$(git rev-parse --abbrev-ref HEAD)
+            ./mend dep -u -r -s "*//\${JOB_NAME}//\${repo}_\${branch}" --fail-policy --non-interactive --export-results dep-results.txt
             """
         } else {
             sh """
-            export repo=$(basename -s .git $(git config --get remote.origin.url))
-            export branch=$(git rev-parse --abbrev-ref HEAD)
-            ./mend dep -u -s "*//${JOB_NAME}//${repo}_${branch}" --fail-policy --non-interactive --export-results dep-results.txt'
+            export repo=\$(basename -s .git \$(git config --get remote.origin.url))
+            export branch=\$(git rev-parse --abbrev-ref HEAD)
+            ./mend dep -u -s "*//\${JOB_NAME}//\${repo}_\${branch}" --fail-policy --non-interactive --export-results dep-results.txt
             """
         }
-
-            // export repo=$(basename -s .git $(git config --get remote.origin.url))
-            // export branch=$(git rev-parse --abbrev-ref HEAD)
-            // if $Reachability; then
-                  
-            // else
-            //        ./mend dep -u -s "*//${JOB_NAME}//${repo}_${branch} --fail-policy" --non-interactive --export-results dep-results.txt
-            // fi
-            sh """
-            if [[ "$dep_exit" == "9" ]]; then
-                  echo "[warning]  Dependency scan policy violation"
-            else
-                  echo "No policy violations found in dependencies scan"
-            fi          
-            """
-      }
-      archiveArtifacts artifacts: "dep-results.txt", fingerprint: true
-
+        sh """
+        if [[ "\${dep_exit}" == "9" ]]; then
+            echo "[warning]  Dependency scan policy violation"
+        else
+            echo "No policy violations found in dependencies scan"
+        fi          
+        """
+    }
+    archiveArtifacts artifacts: "dep-results.txt", fingerprint: true
 }
