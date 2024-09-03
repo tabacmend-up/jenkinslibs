86b665c4569d06edc3ab2efe9765c85ca033dc4f
Ori Tabac (Mend.io) <84838519+tabacmend@users.noreply.github.com>
2024-09-03 20:19:57 +0300

    Update MendSCAScan.groovy

--- vars/MendSCAScan.groovy
+++ vars/MendSCAScan.groovy
@@ -2,17 +2,17 @@ def call(boolean Reachability) {
     echo 'Run Mend dependencies scan'
     catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
         if (Reachability) {
-            sh '''
+            sh """
             export repo=$(basename -s .git $(git config --get remote.origin.url))
             export branch=$(git rev-parse --abbrev-ref HEAD)
             ./mend dep -u -r -s "*//${JOB_NAME}//${repo}_${branch}" --fail-policy --non-interactive --export-results dep-results.txt'
-            '''
+            """
         } else {
-            sh '''
+            sh """
             export repo=$(basename -s .git $(git config --get remote.origin.url))
             export branch=$(git rev-parse --abbrev-ref HEAD)
             ./mend dep -u -s "*//${JOB_NAME}//${repo}_${branch}" --fail-policy --non-interactive --export-results dep-results.txt'
-            '''
+            """
         }
 
             // export repo=$(basename -s .git $(git config --get remote.origin.url))
@@ -22,13 +22,13 @@ def call(boolean Reachability) {
             // else
             //        ./mend dep -u -s "*//${JOB_NAME}//${repo}_${branch} --fail-policy" --non-interactive --export-results dep-results.txt
             // fi
-            sh '''
+            sh """
             if [[ "$dep_exit" == "9" ]]; then
                   echo "[warning]  Dependency scan policy violation"
             else
                   echo "No policy violations found in dependencies scan"
             fi          
-            '''
+            """
       }
       archiveArtifacts artifacts: "dep-results.txt", fingerprint: true
 
