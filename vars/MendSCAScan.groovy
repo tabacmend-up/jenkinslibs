c21ac5774f96c07a3fcbbdfdc7d153f9089a87ca
tabac-mend <ori.tabac@whitesourcesoftware.comgit config --global user.name Ori>
2024-11-06 14:03:38 +0200

    chatgpt

--- vars/MendSCAScan.groovy
+++ vars/MendSCAScan.groovy
@@ -1,25 +1,26 @@
 def call(Map args = [:]) { 
     boolean reachability = args.get('reachability', false)
     echo 'Run Mend dependencies scan'
-    export repo=$(basename -s .git \$(git config --get remote.origin.url))
-    export branch=$(git rev-parse --abbrev-ref HEAD)
+
     catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
-        if (reachability) {
-            sh """
-            ./mend dep -u -r -s "*//\${JOB_NAME}//\${repo}_\${branch}" --fail-policy --non-interactive --export-results dep-results.txt
-            dep_exit=\$?
-            """
-        } else {
-            sh """
-            // export repo=\$(basename -s .git \$(git config --get remote.origin.url))
-            // export branch=\$(git rev-parse --abbrev-ref HEAD)
-            ./mend dep -u -s "*//\${JOB_NAME}//\${repo}_\${branch}" --fail-policy --non-interactive --export-results dep-results.txt
-            dep_exit=\$?
-            """
-        }
         sh """
+        export repo=\$(basename -s .git \$(git config --get remote.origin.url))
+        export branch=\$(git rev-parse --abbrev-ref HEAD)
+        """
+
+        def mendCommand = "./mend dep -u -s \"*//\${JOB_NAME}//\${repo}_\${branch}\" --fail-policy --non-interactive --export-results dep-results.txt"
+        
+        if (reachability) {
+            mendCommand += " -r" // Add reachability flag
+        }
+
+        // Execute the Mend command
+        sh """
+        ${mendCommand}
+        
+        dep_exit=\$?
         if [[ "\$dep_exit" == "9" ]]; then
-            echo "[warning]  Dependency scan policy violation"
+            echo "[warning] Dependency scan policy violation"
         else
             echo "No policy violations found in dependencies scan"
         fi
